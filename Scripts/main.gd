5f8175c60b6eac5ee272cc31ea12ab7635cec964
Dorfultariant <tunxero@hotmail.com>
2025-07-01 19:20:24 +0300

    Huoh

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -9,7 +9,7 @@ var score_file = "user://highscore.txt"
 var start_time : int = Time.get_unix_time_from_system()
 
 func update_score() -> void:
-		LBL_SCORE.text = "Score: {0}".format([score_counter]) # Replace with function body.
+		LBL_SCORE.text = "Score: {0}".format([score_counter]) 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,24 +24,24 @@ func _process(_delta: float) -> void:
 			if $Player.global_position.y > -20:
 					#score_counter += Time.get_ticks_msec()
 					#score_counter /= 1000
-					score_counter = (Time.get_unix_time_from_system() - start_time)
+					score_counter = int((Time.get_unix_time_from_system() - start_time))
 					update_score()
 			else:
 				$Player.queue_free()
 				check_highscore()
 
-func load_highscore() -> int:
+func load_highscore():
 		var fd = FileAccess.open(score_file, FileAccess.READ)
 		if fd == null:
 				write_highscore(0)
 				return 0
-		var c64 = fd.get_64()
+		var d64 = fd.get_double()
 		fd.close()
-		return c64
+		return d64
 
-func write_highscore(score: int) -> void:
+func write_highscore(score) -> void:
 		var fd = FileAccess.open(score_file, FileAccess.WRITE)
-		fd.store_64(score)
+		fd.store_double(score)
 		fd.close()
 
 func check_highscore() -> void:
